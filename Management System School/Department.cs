55cd54657207a404bbf3ba6c9810edcebacc330f
visal <salkizzyou@gmail.com>
2025-12-25 22:51:54 +0700

    Add some code in Department and Student

--- Management System School/Department.cs
+++ Management System School/Department.cs
@@ -8,5 +8,20 @@ namespace Management_System_School
 {
     internal class Department
     {
+        private static int counter = 0;
+        public string DeptId { get; }
+        private string deptName;
+        public string DeptName
+        {
+            get => deptName;
+            set => deptName = value ?? throw new ArgumentNullException(nameof(value));
+        }
+        public Department(string DeptName)
+        {
+            DeptId = $"DP{++counter:D3}";
+            this.DeptName = DeptName ?? throw new ArgumentNullException(nameof(DeptName));
+        }
+        List<Student> students = new List<Student>();
+        List<Professor> professors = new List<Professor>();
     }
 }
--- Management System School/Student.cs
+++ Management System School/Student.cs
@@ -8,5 +8,39 @@ namespace Management_System_School
 {
     internal class Student
     {
+        private static int conuter = 0;
+        private string StdId;
+        private string stdName;
+        public string StdName
+        {
+            get => stdName;
+            set => stdName = value ?? throw new ArgumentNullException(nameof(value));
+        }
+        private int stdAge;
+        public int StdAge
+        {
+            get => stdAge;
+            set
+            {
+                if (value < 18)
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                StdAge = value;
+            }
+        }
+        private string stdAddress;
+        public string StdAddress
+        {
+            get => stdAddress;
+            set => stdAddress = value ?? throw new ArgumentNullException(nameof(value));
+        }
+        public Student(string StdName, int StdAge, string StdAddress, Department dept)
+        {
+            StdId = $"ST {++conuter:D3}";
+            this.StdName = StdName;
+            this.StdAge = StdAge;
+            this.StdAddress = StdAddress;
+            this.Department = dept;
+        }
+        public Department Department { get; private set; }
     }
 }
